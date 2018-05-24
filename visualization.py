d934bad5f7094a8b4fb7e73faf82b63cec8b4922
sundariam <37808047+sundariam@users.noreply.github.com>
2018-05-24 15:53:27 +0300

    variables' names fixed

--- visualization.py
+++ visualization.py
@@ -4,32 +4,32 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 
-PARSER = argparse.ArgumentParser()
-PARSER.add_argument('--model', default='model.txt')
-PARSER.add_argument('--model2', default='model2.txt')
-ARGS = PARSER.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', default='model.txt')
+parser.add_argument('--model2', default='model2.txt')
+args = parser.parse_args()
 
 
-with open(ARGS.model, "rb") as file:
-    RESULT1 = pickle.load(file)
+with open(args.model, "rb") as file:
+    result1 = pickle.load(file)
 
-with open(ARGS.model2, "rb") as file:
-    RESULT2 = pickle.load(file)
+with open(args.model2, "rb") as file:
+    result2 = pickle.load(file)
 
-DATA_NAMES = ['средняя зарплата > 25000', 'средняя зарплата от 25000 до 35000',
+data_names = ['средняя зарплата > 25000', 'средняя зарплата от 25000 до 35000',
               'средняя зарплата < 25000']
-DATA_VALUES = RESULT1
+data_values = result1
 
 
 def show_pie():
     """строит круговую диаграмму"""
     plt.title('Распределение зарплат по России')
 
-    plt.pie(DATA_VALUES, autopct='%.1f', radius=1.1,
-            explode=[0.10] + [0 for _ in range(len(DATA_NAMES) - 1)])
+    plt.pie(data_values, autopct='%.1f', radius=1.1,
+            explode=[0.10] + [0 for _ in range(len(data_names) - 1)])
     plt.legend(
         bbox_to_anchor=(-0.16, 0.15, 0.25, 0.25),
-        loc='lower left', labels=DATA_NAMES)
+        loc='lower left', labels=data_names)
     plt.show()
 
 
@@ -38,7 +38,7 @@ def show_columns():
     regions = []
     y_column = []
     plt.title('10 регионов с самой высокой зарплатой')
-    for i in RESULT2:
+    for i in result2:
         regions.append(i[0])
         y_column.append(i[1])
 
